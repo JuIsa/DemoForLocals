f35e388582d884ccf6b34cdc66ed1101fc97cfde
Isa <isami.dono@gmail.com>
2020-09-29 16:32:40 +0300

    Demo: - Add Vuoforia; - Buttons: turn on-off house;

---
+++ Demo AR/Assets/Scripts/House/btnCtrl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnCtrl : MonoBehaviour
+{
+    public GameObject texts;
+    public GameObject furniture;
+    public GameObject doors;
+
+    private bool isTextOn = true;
+    private bool isFurOn = true;
+    private bool isDoorsOn = true;
+    
+
+    public void textClicked()
+    {
+        if (isTextOn) {
+            texts.SetActive(false);
+            isTextOn = !isTextOn;
+        }
+        else
+        {
+            texts.SetActive(true);
+            isTextOn = !isTextOn;
+        }
+            
+    }
+
+    public void furnitureCliked()
+    {
+        if (isFurOn)
+        {
+            furniture.SetActive(false);
+            isFurOn = !isFurOn;
+        }
+        else
+        {
+            furniture.SetActive(true);
+            isFurOn = !isFurOn;
+        }
+    }
+
+    public void doorsClicked()
+    {
+        if (isDoorsOn)
+        {
+            doors.SetActive(false);
+            isDoorsOn = !isDoorsOn;
+        }
+        else
+        {
+            doors.SetActive(true);
+            isDoorsOn = !isDoorsOn;
+        }
+    }
+}
