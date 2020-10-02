5066332023382508c3344458cf6688c20c556578
Isa <isami.dono@gmail.com>
2020-10-02 03:06:52 +0300

    Demo: - New folders; - nesq ar found behaviour;

---
+++ Demo AR/Assets/Nesquik/nesqCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nesqCtrl : MonoBehaviour
+{
+    public GameObject smallPieces;
+    public Animator bigPiece;
+    public detection dtctnScript;
+    private void Start()
+    {
+        dtctnScript.targetDetected += ActivateSmallRigid;
+    }
+
+    private void ActivateSmallRigid()
+    {
+        Rigidbody[] rbs = smallPieces.GetComponentsInChildren<Rigidbody>();
+        foreach (var rb in rbs)
+            rb.isKinematic = false;
+        dtctnScript.targetDetected -= ActivateSmallRigid;
+        StartCoroutine(PlayBigAni());
+    }
+
+    private IEnumerator PlayBigAni()
+    {
+        yield return new WaitForSeconds(2f);
+        bigPiece.Play("open");
+    }
+}
