114fe7ccb2922f437a5f525e7d365c1e05938319
Isa <isami.dono@gmail.com>
2020-10-05 00:46:30 +0300

    Demo: - Cola;

---
+++ Demo AR/Assets/Cola/Scripts/colaCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colaCtrl : MonoBehaviour
+{
+    public Animator cola;
+    public cola_detection dtcnScript;
+
+    void Start()
+    {
+        dtcnScript.onTargetFound += StartAnimation;
+        dtcnScript.onTargetLost += StopAnimation;
+            
+    }
+
+    private void StartAnimation()
+    {
+        cola.Play("drive");
+        cola.StartPlayback();
+    }
+
+    private void StopAnimation()
+    {
+        cola.StopPlayback();
+    }
+
+
+
+}
